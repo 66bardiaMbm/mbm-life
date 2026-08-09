7cc744b0b0aa3838a7584eb9ed6448d1613ff1d2
66bardiaMbm <66bardia@gmail.com>
2026-08-09 11:03:39 +1000

    Update build.gradle.kts

--- android-app/app/build.gradle.kts
+++ android-app/app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.mbmlife.companion"
         minSdk = 26
         targetSdk = 35
-        versionCode = 18
-    versionName = "0.9.0-BOOTTEST"
+       versionCode = 19
+versionName = "1.0.0-MAINFIX"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField(
